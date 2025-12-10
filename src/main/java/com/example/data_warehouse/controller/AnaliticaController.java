532c5c3376a6bac01d305747785109497ef96078
efejotax08 <efejotax08@gmail.com>
2025-12-09 23:01:31 -0500

    Apis

--- src/main/java/com/example/data_warehouse/controller/AnaliticaController.java
+++ src/main/java/com/example/data_warehouse/controller/AnaliticaController.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/analitica")
+@RequestMapping("/api/analytics")
 public class AnaliticaController {
 
     private final AnaliticaService analiticaService;
